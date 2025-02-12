7130d6501d683d96f543490e23bf8d79a8679947
hai lt <Tuanhai13112k3@gmail.com>
2025-02-12 14:58:20 +0700

    searchbar

--- ClickCart/Pages/Products/Index.cshtml.cs
+++ ClickCart/Pages/Products/Index.cshtml.cs
@@ -22,7 +22,6 @@ namespace ClickCart.Pages.Customer.Products
 		public int CurrentPage { get; set; }  // Trang hiện tại
 		public int TotalPages { get; set; }  // Tổng số trang
 		public int CategoryId { get; set; }  // Id của danh mục hiện tại
-		public string ProductName { get; set; }  // Tìm kiếm theo tên sản phẩm
 
 		public async Task OnGetAsync(int? categoryId, string name, string price, int pageNumber = 1)
 		{
@@ -31,24 +30,26 @@ namespace ClickCart.Pages.Customer.Products
 			HttpContext.Session.SetInt32("CategoryId", categoryId ?? 1);
 			await LoadProductsAsync(categoryId, name, price, pageNumber);
 		}
-		//public async Task OnPostAsync(string name, string price, int pageNumber = 1)
-		//{
-		//	Categories = await _context.Categories.ToListAsync();
-		//	int categoryId = HttpContext.Session.GetInt32("CategoryId") ?? 1;
-		//	await LoadProductsAsync(categoryId, name, price, pageNumber);
-		//}
+		public async Task OnPostAsync( int pageNumber = 1)
+		{
+			Categories = await _context.Categories.ToListAsync();
+			int categoryId = HttpContext.Session.GetInt32("CategoryId") ?? 1;
+			string name = Request.Form["search"];
+			string price = Request.Form["priceRange"];
+			await LoadProductsAsync(categoryId, name, price, pageNumber);
+		}
 		private async Task LoadProductsAsync(int? categoryId, string name, string price, int pageNumber)
 		{
 
-			ProductName = name ?? string.Empty;
+			
 			const int pageSize = 6;
 			var queryProducts = _context.Products.AsQueryable();
 
 			queryProducts = queryProducts.Where(p => p.CategoryID == CategoryId);
 
-			if (!string.IsNullOrEmpty(ProductName))
+			if (!string.IsNullOrEmpty(name))
 			{
-				queryProducts = queryProducts.Where(p => p.ProductName.Contains(ProductName));
+				queryProducts = queryProducts.Where(p => p.ProductName.Contains(name));
 			}
 
 			if (!string.IsNullOrEmpty(price))
