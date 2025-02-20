6af038078abfa86e2d9db9ede57f4a9f808fe762
conght <conghthe172673@fpt.edu.vn>
2025-02-21 01:52:58 +0700

    add Premium Table

---
+++ ClickCart/Models/Premium.cs
@@ -0,0 +1,11 @@
+﻿namespace ClickCart.Models
+{
+    public class Premium
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string PaymentStatus { get; set; }
+        public string PremiumName { get; set; }
+        public int Price { get; set; }
+    }
+}
